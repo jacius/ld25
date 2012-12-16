871a8c938544f78860b7a52bcc208ebb8bd9e18e
Shawn Anderson <shawn42@gmail.com>
2012-12-15 20:03:09 -0500

    better font

--- src/intro_stage.rb
+++ src/intro_stage.rb
@@ -1,6 +1,6 @@
 define_stage :intro do
   curtain_up do
-    create_actor :label, x: 200, y: 200, text: "MOAR AXE", font_name: 'docseuss.ttf', font_size: 140, color: Color::YELLOW
+    create_actor :label, x: 250, y: 150, text: "MOAR AXE", font_name: 'doctor_soos_bold.ttf', font_size: 200, color: Color::YELLOW
     input_manager.reg :down do 
       fire :next_stage
     end
